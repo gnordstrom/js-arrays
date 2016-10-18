0f2021d42bac7d644f6b85002fca311149b05f94
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-10-17 22:17:35 -0600

    1/4 of way done

--- arraysPractice.js
+++ arraysPractice.js
@@ -5,7 +5,9 @@ var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
 
   //Code Here
-
+function first(arr) {
+	return arr[0];
+}
 
 //Next problem
 
@@ -13,7 +15,10 @@ var arr = [10,20,30];
 
 var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item in the given array.
-
+function last(arr) {
+	var last = arr.length - 1;
+	return arr[last];
+}
 
   //Code Here
 
@@ -23,7 +28,11 @@ var arr = [40,50,60];
 
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
-
+function looper(family) {
+	for (var i = 0; i < family.length; i++) {
+		alert(family[i]);
+	}
+}
   //Code Here
 
 
@@ -33,7 +42,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
-
+function reversedLooper(letters) {
+	for (var i = letters.length - 1; i >= 0; i--) {
+		alert(letters[i]);
+	}
+}
   //Code Here
 
 
@@ -42,7 +55,14 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
-
+function evenFinder(nums) {
+	for (var i = 0; i < nums.length; i++) {
+		if (!nums[i]/2) {
+			nums.splice(i, 1);
+		}
+	}
+	return nums;
+}
   //Code Here
 
 
